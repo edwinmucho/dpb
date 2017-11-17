512c7291e5492ca92b23e28e4baa253713825354
ed <edwin.mucho@gmail.com>
2017-11-18 04:55:02 +0900

    change

--- api/index.js
+++ api/index.js
@@ -19,15 +19,15 @@
 console.log('APIs initialize');
 
 const conversation = require('./message');
-//const kakao_keyboard = require('./kakao/keyboard');
-//const kakao_message = require('./kakao/message');
+const kakao_keyboard = require('./kakao/keyboard');
+const kakao_message = require('./kakao/message');
 
 
 module.exports = {
     'initialize': (app, options) => {
         conversation.initialize(app, options);
-        //kakao_keyboard.initialize(app, options);
-        //kakao_message.initialize(app, options);
+        kakao_keyboard.initialize(app, options);
+        kakao_message.initialize(app, options);
         
     }
 };
